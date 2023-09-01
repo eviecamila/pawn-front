717e88cfb2b48afb614acc3167030949b37856d8
minminaburrida <pcvvblah@gmail.com>
2023-08-31 18:06:34 -0600

    modified:   src/app/app-routing.module.ts 	modified:   src/app/app.module.ts 	deleted:    src/app/components/index/index.component.css 	deleted:    src/app/components/index/index.component.html 	deleted:    src/app/components/index/index.component.spec.ts 	deleted:    src/app/components/index/index.component.ts 	deleted:    src/app/components/notfound/notfound.component.css 	deleted:    src/app/components/notfound/notfound.component.html 	deleted:    src/app/components/notfound/notfound.component.spec.ts 	deleted:    src/app/components/notfound/notfound.component.ts 	deleted:    src/app/components/template/template.component.css 	deleted:    src/app/components/template/template.component.html 	deleted:    src/app/components/template/template.component.spec.ts 	deleted:    src/app/components/template/template.component.ts 	modified:   src/app/templates/actions/actions.component.css 	modified:   src/app/templates/actions/actions.component.html 	modified:   src/app/templates/actions/actions.component.ts 	modified:   src/app/templates/top-bar/top-bar.component.html 	modified:   src/styles.css 	src/app/paths/ 	src/app/templates/img/ 	src/assets/nu.svg 	src/assets/tdc.png

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,21 @@
+// Base imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { IndexComponent } from './components/index/index.component';
-import { NotfoundComponent } from './components/notfound/notfound.component';
+
+// TEMPLATES
+import { IndexComponent } from './paths/index/index.component';
+import { NotfoundComponent } from './paths/notfound/notfound.component';
+import { NewAccountComponent } from './paths/new-account/new-account.component';
+import { ProductsComponent } from './paths/products/products.component';
 const routes: Routes = [
-  // {
-  //   path: 'index/',
-  //   loadChildren: () => import('./components/index/index.component').then(x => x.IndexComponent),
-  //   canActivate: [GuardGuard]
+  {path: 'index',component: IndexComponent,},
+  {path: '404',component: NotfoundComponent,},
+  {path: '',redirectTo: 'index',pathMatch: 'full',},
+  {path: 'newaccount',component: NewAccountComponent},
+  {path: 'products',component: ProductsComponent},
 
-  // },
-  {
-    path: 'index',
-    component: IndexComponent,
-    // canActivate: [GuardGuard]
 
-  },
-  {
-    path: '404',
-    component: NotfoundComponent,
-
-  },
-  {
-    path: '',
-    redirectTo: 'index',
-    pathMatch: 'full',
-
-  },
-  {
-    path: '**',
-    redirectTo: '404',
-    pathMatch: 'full',
-
-  },
+  {path: '**',redirectTo: '404',pathMatch: 'full',},
 ];
 
 @NgModule({
--- src/app/templates/actions/actions.component.ts
+++ src/app/templates/actions/actions.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ActionsComponent implements OnInit {
   @Input() content!: string;
+  @Input() url!: string;
   constructor() { }
 
   ngOnInit(): void {
