64bfcd3e7d95321573edfc2084ff9c709b45632a
MinMinAburrida <pcvvblah@gmail.com>
2023-11-03 22:59:57 -0700

    Simulador de prestamo
    when eres tonta (some changes :nail_care: )
    tenemos que modificarlo y hacerlo funcional

--- src/app/paths/pawn/pawn.component.ts
+++ src/app/paths/pawn/pawn.component.ts
@@ -1,39 +1,46 @@
-import { Component, OnInit} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { PawnService } from 'src/app/services/pawn.service';
 
-// Pruebas tontas unitarias
-import { HiworldService } from 'src/app/services/hiworld.service';
 @Component({
   selector: 'app-pawn',
   templateUrl: './pawn.component.html',
   styleUrls: ['./pawn.component.css'],
-  providers:[PawnService]
+  providers: [PawnService],
 })
 export class PawnComponent implements OnInit {
-  constructor(
-    private pawn: PawnService,
-   private  hi: HiworldService,
-    ){}
-  monto: number = 0;
-  tasa: number = 0;
-  plazo: number = 0;
-  resultado: number = 0;
+  tipoItem: string = 'Selecciona un tipo de ítem';
+  dias!: number;
+  monto!: number;
+  tiposItem: any[] = []; // Inicializa como un arreglo vacío
 
-  tiposItem!:any;
+  constructor(private pawn: PawnService) {}
+
   ngOnInit(): void {
-    this.pawn.tiposItem().subscribe(data => {this.tiposItem = data;console.log(this.tiposItem);});
-    this.hi.wasa().subscribe(data => {console.log(data)});
+    this.pawn.tiposItem().subscribe((data: any) => {
+      this.tiposItem = data.tipos_item; // Asigna el arreglo de tipos de item
+    });
   }
+
   calcularCuota() {
-    // Convierte la tasa de interés de porcentaje a fracción decimal
-    const tasaDecimal = this.tasa / 100;
+    // Aquí puedes realizar los cálculos según tus necesidades
+    // Por ejemplo, calcula la cuota de empeño con la lógica deseada
+    const tasaDeInteres = this.getTasaDeInteresPorTipoItem(this.tipoItem);
+    // Realiza tus cálculos aquí con la tasa de interés y otros datos
+    const interes = (this.monto) * (tasaDeInteres*.01) * (this.dias / 365),
+    importe=this.monto+interes;
 
-    // Calcula la cuota mensual
-    const cuota = (this.monto * tasaDecimal) / (1 - Math.pow(1 + tasaDecimal, -this.plazo));
+    // tasa_decimal = cache['tasa'] / 100.0 * 3 * 1.1
 
-    // Actualiza el resultado
-    this.resultado = cuota;
-    console.log(this.resultado);
+    // Puedes mostrar la cuota en la consola o en una variable, según tus necesidades
+    console.log(`Capital inicial  : ${Number(this.monto.toFixed(2))}`);
+    console.log(`Capital final    : ${Number(importe.toFixed(2))}`);
+    console.log(`Intereses totales: ${Number(interes.toFixed(2))}`);
+  }
+
+  getTasaDeInteresPorTipoItem(tipo: string): number {
+    // Puedes implementar una función que obtenga la tasa de interés según el tipo de item
+    // Recorre this.tiposItem para encontrar el objeto que coincide con el tipo
+    const tipoEncontrado = this.tiposItem.find((item) => item.tipo === tipo);
+    return tipoEncontrado ? tipoEncontrado.tasa_interes : 0;
   }
 }
