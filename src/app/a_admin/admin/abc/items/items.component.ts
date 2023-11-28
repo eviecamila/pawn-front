97fa8aa6d96887358373091e082d933e81357443
Evie Pussifresh <pcvvblah@gmail.com>
2023-11-27 19:05:10 -0700

    soy quirino
    cambios

--- src/app/a_admin/admin/abc/items/items.component.ts
+++ src/app/a_admin/admin/abc/items/items.component.ts
@@ -18,13 +18,34 @@ export class ItemsComponent {
     info:true,
     add:true
   }
+  @Input() found: any = [];
+  // ngOnInit(): void {
+  //   this.data.emit({
+  //     icon: 'headset',
+  //     description: 'Gestión de empleados, incluyendo agregar, dar de baja y modificar información del empleado.',
+  //     type: 'Empleados',
+  //     response: 'empleados',
+  //     forms: this.forms,
+  //     searchPlaceholder: 'RFC: PPLU020222HD3',
+  //     searchLength: 13,
+  //     url: '/rrhh/',
+  //     urls: { 'search': '?rfc=' },
+  //     buttons: {
+  //       edit: true,
+  //       delete: true,
+  //     }
+  //   });
+  // }
   ngOnInit(): void {
     this.data.emit({
       icon: 'gem',
       description: 'Inventario de pertenencias, para agregar y manejar los artículos disponibles.',
       type: 'Pertenencias',
       forms:this.forms,
-      searchPlaceholder: 'Inserta el ID de la pertenencia'
+      searchPlaceholder: 'Inserta el ID de la pertenencia',
+      searchLength: 13,
+      url: '/items/',
+      urls: { 'search': '?id=' },
     });
   }
 }
