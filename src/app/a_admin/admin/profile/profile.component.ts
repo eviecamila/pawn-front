e90e0201cacafcaa4bb0e826f13e16b11b889b4e
MinMinAburrida <pcvvblah@gmail.com>
2023-12-25 18:43:59 -0800

    Mi perfil =) (Falta la password)

--- src/app/a_admin/admin/profile/profile.component.ts
+++ src/app/a_admin/admin/profile/profile.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
+import { EmployeeService } from 'src/app/services/employee.service';
+
+
+declare var $: any;
+
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -9,15 +16,48 @@ export class ProfileComponent {
 
   isFormEditable: boolean = true;
 
-  toggleFormEdit() {
-    this.isFormEditable = !this.isFormEditable;
+  toggleFormEdit() {this.isFormEditable = !this.isFormEditable;}
+  editEmployee() {
+    if (this.newEmployee !== this.newEmployee2) {
+      $.blockUI({
+        message: '<div style="display:grid;place-items:center"><img width="15%" src="assets/cagando.gif" style="position:relative;right:0%;top:30%"></div>',
+        overlayCSS: {
+          backgroundColor: '#1b2024',
+          opacity: 0.8,
+          zIndex: 1200,
+          cursor: 'wait',
+        }, css: {
+          border: 0,
+          color: '#fff',
+          padding: 0,
+          zIndex: 1201,
+          backgroundColor: 'transparent',
+        },
+      });
+      // console.log(this.newEmployee)
+      this.employee.edit(this.newEmployee).subscribe((r: any) => {
+        // Mensaje
+        // console.log(r)
+        if (r.status === "OK") { this.toastr.success(r.message); }
+        else { this.toastr.error(r.message); }
+        setTimeout(() => {
+          $.unblockUI();
+          location.reload();
+        }, 2000);
+      })
+    }
+    else{
+      this.toastr.warning("No se modificó nada");
+      this.toggleFormEdit();
+    }
   }
-  newEmployee = {
+  newEmployee: any = {
     id: 0,
     nombre: "",
     apellido1: "",
     apellido2: "",
     rfc: "",
+    RFC: "",
     nss: "",
     telefono: "",
     puesto: "",
@@ -25,13 +65,34 @@ export class ProfileComponent {
     salario: 0,
     // Agrega valores para otros campos si es necesario
   };
+  newEmployee2: any = {};
 
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private employee: EmployeeService,
+    private toastr: ToastrService
   ) {
+    $.blockUI({
+      message: '<div style="display:grid;place-items:center"><img width="15%" src="assets/cagando.gif" style="position:relative;right:0%;top:30%"></div>',
+      overlayCSS: {
+        backgroundColor: '#1b2024',
+        opacity: 0.8,
+        zIndex: 1200,
+        cursor: 'wait',
+      }, css: {
+        border: 0,
+        color: '#fff',
+        padding: 0,
+        zIndex: 1201,
+        backgroundColor: 'transparent',
+      },
+    });
     this.auth.currentUser(localStorage.getItem('token')).subscribe((res: any) => {
       this.newEmployee = res
+      this.newEmployee['RFC'] = res.rfc
+      this.newEmployee2 = this.newEmployee;
     });
+    $.unblockUI()
   }
 
 }
---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+const apiURL = environment
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  apiURL: string = `${environment.address}/rrhh/`;
+
+  // Editar datos de empleado
+  edit(data:any){return this.http.post(`${this.apiURL}employee/edit/`, data);}
+
+  constructor(private http: HttpClient) { }
+}
